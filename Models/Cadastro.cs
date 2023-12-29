3845f39ca4e153fa9e02fc127990fe49e6ce2c79
Leonardo buta <teste@teste.com>
2023-12-29 00:20:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Cadastro.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeladeCadastro.Models
+{
+    [Table("Cadastro")]
+    public class Cadastro
+    {
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string nome { get; set; }
+
+
+        [Column("CPF")]
+        [Display(Name = "CPF")]
+        public int cpf { get; set; }
+
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+
+        [Column("Idade")]
+        [Display(Name = "Idade")]
+        public int idade { get; set; }
+
+
+        [Column("Faixa")]
+        [Display(Name = "Faixa")]
+
+        public string faixa { get; set; }
+    }
+}
---
+++ Models/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeladeCadastro.Models
+{
+    public class Contexto : DbContext 
+    {
+        public Contexto(DbContextOptions<Contexto>options) : base(options)
+        {
+        }
+        public DbSet<Cadastro> cadastros { get; set; }
+    }
+}
